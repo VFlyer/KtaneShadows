99132ea281ec3e5c573a13f249d2196f2a5f46aa
BakersDozenBagels <Bakersdozenbagelsbergman@gmail.com>
2022-04-02 23:45:15 -0400

    [V1.0] Initial Commit

---
+++ Assets/Modules/Brown/BrownButtonScript.cs
@@ -0,0 +1,356 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using BrownButton;
+using UnityEngine;
+using Rnd = UnityEngine.Random;
+
+public class BrownButtonScript : MonoBehaviour
+{
+    public KMBombModule Module;
+    public KMBombInfo BombInfo;
+    public KMAudio Audio;
+    public KMSelectable BrownButtonSelectable;
+    public GameObject BrownButtonCap;
+    public MeshRenderer WideMazeScreen;
+    public Camera WideMazeCamera;
+    public Transform WallsParent;
+    public GameObject WallTemplate;
+    public GameObject Camera;
+    public Material[] Materials;
+
+    public KMSelectable[] OtherButtons;
+
+    private static int _moduleIdCounter = 1;
+    private int _moduleId;
+    private bool _moduleSolved;
+    private bool _moduleActivated = false;
+    List<Vector3Int> _chosenNet;
+    private Vector3 _currentRotation;
+    private Vector3Int _currentPosition;
+    private Coroutine _moveRoutine = null;
+    Dictionary<Vector3Int, Ax> _absoluteAxes;
+    private Ax _correctAxis;
+    private Quaternion _trueRotation = Quaternion.identity;
+
+    private const int REPEATSCOUNT = 1;
+
+    private void Start()
+    {
+        _moduleId = _moduleIdCounter++;
+        BrownButtonSelectable.OnInteract += BrownButtonPress;
+        for(int i = 0; i < OtherButtons.Length; ++i)
+        {
+            int j = i;
+            OtherButtons[i].OnInteract += () => { OtherPress(j); return false; };
+        }
+        BrownButtonSelectable.OnInteractEnded += BrownButtonRelease;
+        WideMazeScreen.material.mainTexture = new RenderTexture(WideMazeScreen.material.mainTexture as RenderTexture);
+        WideMazeCamera.targetTexture = WideMazeScreen.material.mainTexture as RenderTexture;
+
+        panic:
+
+        List<Vector3Int> changes = new List<Vector3Int> { new Vector3Int(1, 0, 0), new Vector3Int(-1, 0, 0), new Vector3Int(0, 1, 0), new Vector3Int(0, -1, 0), new Vector3Int(0, 0, 1), new Vector3Int(0, 0, -1) };
+        _chosenNet = new List<Vector3Int> { new Vector3Int(0, 0, 0) };
+        List<Or> searchSpace = new List<Or>();
+        Ax[] orig = new Ax[8] { Ax.Left, Ax.Front, Ax.Back, Ax.Right, Ax.Up, Ax.Zig, Ax.Down, Ax.Zag };
+        _absoluteAxes = new Dictionary<Vector3Int, Ax> { { new Vector3Int(0, 0, 0), Ax.Down } };
+
+        foreach(Vector3Int change in changes)
+            searchSpace.Add(new Or { Pos = change, Axes = orig.RotateFromChange(change) });
+
+        List<Ax> claimed = new List<Ax> { Ax.Down };
+        int repeatCount = 0;
+
+        while(claimed.Count < 8)
+        {
+            IEnumerable<Or> allOrs = searchSpace.Where(o => o.Axes != null && !claimed.Contains(o.Axes[6]) && !_chosenNet.Contains(o.Pos));
+            if(allOrs.Count() == 0)
+            {
+                Debug.LogFormat("<Brown's Shadow #{0}> Panicked!", _moduleId);
+                goto panic;
+            }
+            Or newOr = allOrs.PickRandom();
+            _chosenNet.Add(newOr.Pos);
+            _absoluteAxes.Add(newOr.Pos, newOr.Axes[6]);
+            claimed.Add(newOr.Axes[6]);
+
+            foreach(Vector3Int change in changes)
+            {
+                Or created = new Or { Pos = newOr.Pos + change, Axes = newOr.Axes.RotateFromChange(change) };
+
+                foreach(Or or in searchSpace.Where(o => o.Pos == created.Pos))
+                    or.Axes = null;
+
+                if(!searchSpace.Any(o => o.Pos == created.Pos))
+                    searchSpace.Add(created);
+            }
+
+            if(changes.Count(c => _chosenNet.Contains(newOr.Pos + c)) >= 5)
+            {
+                foreach(Vector3Int change in changes)
+                {
+                    foreach(Or or in searchSpace.Where(o => o.Pos == newOr.Pos + change))
+                        or.Axes = null;
+                }
+            }
+
+            foreach(Or or in searchSpace.Where(o => o.Pos == newOr.Pos))
+                or.Axes = null;
+
+            if(claimed.Count == 8 && repeatCount++ < REPEATSCOUNT)
+                claimed = new List<Ax>();
+        }
+
+        int ansAx = Rnd.Range(0, 4);
+        Debug.LogFormat("<Brown's Shadow #{0}> The solution axis is id {1}.", _moduleId, ansAx);
+
+        List<int> ixes = new List<int> { 1, 9, 17 }.Shuffle();
+        ixes.Insert(ansAx, 0);
+
+        List<Ax> markedAxes = new List<Ax>();
+
+        List<Ax[]> alltests = new List<Ax[]> {
+            new Ax[] { Ax.Up, Ax.Down },
+            new Ax[] { Ax.Left, Ax.Right },
+            new Ax[] { Ax.Front, Ax.Back },
+            new Ax[] { Ax.Zig, Ax.Zag }
+        };
+
+        Debug.LogFormat("[Brown's Shadow #{0}] Chose net: {1}", _moduleId, _chosenNet.Select(v => v.ToString()).Join(", "));
+        for(int ix = 0; ix < _chosenNet.Count; ix++)
+            Debug.LogFormat("[Brown's Shadow #{0}] Cube {1} corresponds to {2}.", _moduleId, _chosenNet[ix], (int)_absoluteAxes[_chosenNet[ix]]);
+
+        for(int i = 0; i < alltests.Count; i++)
+        {
+            Ax[] tests = alltests[i];
+            IEnumerable<Vector3Int> nodes = _chosenNet.Where(p => _absoluteAxes[p] == tests[0] || _absoluteAxes[p] == tests[1]);
+            Vector3Int marked = nodes.PickRandom();
+            if(ansAx != i)
+                markedAxes.Add(_absoluteAxes[marked]);
+            Vector3Int markedWall = changes.Where(c => !_chosenNet.Contains(marked + c)).PickRandom();
+
+            AddWall(marked, markedWall, ansAx == i ? Materials[0] : Materials[ixes[i] + (int)_absoluteAxes[marked]]);
+            if(ansAx != i)
+            {
+                Debug.LogFormat("[Brown's Shadow #{0}] Cube {1} is displaying {2}.", _moduleId, marked, Materials[ixes[i] + (int)_absoluteAxes[marked]].name.Substring(7));
+            }
+
+            foreach(Vector3Int change in changes.Where(c => !_chosenNet.Contains(marked + c) && c != markedWall))
+                AddWall(marked, change, Materials[0]);
+
+            foreach(Vector3Int pos in nodes.Where(n => n != marked))
+            {
+                foreach(Vector3Int change in changes.Where(c => !_chosenNet.Contains(pos + c)))
+                    AddWall(pos, change, Materials[0]);
+            }
+        }
+
+        if(alltests.Select(a => a[0]).Count(a => markedAxes.Contains(a)) >= 2)
+            _correctAxis = alltests[ansAx][0];
+        else
+            _correctAxis = alltests[ansAx][1];
+
+        Debug.LogFormat("[Brown's Shadow #{0}] The correct cell to submit is {1}.", _moduleId, _chosenNet.First(c => _absoluteAxes[c] == _correctAxis));
+
+        _currentPosition = _chosenNet.PickRandom();
+        _currentRotation = new Vector3Int(0, 0, 0);
+        Vector3 end = new Vector3(_currentPosition.x, _currentPosition.y, _currentPosition.z) * -0.1f - _currentRotation * 0.1f;
+        WallsParent.localPosition = end;
+
+        GetComponentInChildren<CameraScript>().UpdateChildren();
+        Module.OnActivate += delegate
+        {
+            _moduleActivated = true;
+        };
+    }
+
+    private void OtherPress(int j)
+    {
+        Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, OtherButtons[j].transform);
+        OtherButtons[j].AddInteractionPunch();
+
+        switch(j)
+        {
+            case 4:
+                _trueRotation *= Quaternion.Euler(0f, 0f, 90f);
+                break;
+            case 0:
+                _trueRotation *= Quaternion.Euler(0f, -90f, 0f);
+                break;
+            case 1:
+                _trueRotation *= Quaternion.Euler(0f, 0f, -90f);
+                break;
+            case 3:
+                _trueRotation *= Quaternion.Euler(90f, 0f, 0f);
+                break;
+            case 2:
+                _trueRotation *= Quaternion.Euler(0f, 90f, 0f);
+                break;
+            case 5:
+                _trueRotation *= Quaternion.Euler(-90f, 0f, 0f);
+                break;
+        }
+
+        StartCoroutine(RotateCamera());
+    }
+
+    private class Or
+    {
+        public Ax[] Axes;
+        public Vector3Int Pos;
+    }
+
+    public enum Ax
+    {
+        Left,
+        Front,
+        Back,
+        Right,
+        Up,
+        Zig,
+        Down,
+        Zag
+    }
+
+    private const float DELAY_A = 0.5f;
+    private int _cameraPresses = 0;
+
+    private IEnumerator RotateCamera()
+    {
+        Vector3 pointing = _trueRotation * Vector3.down;
+        int presses = ++_cameraPresses;
+        if(pointing.x > 0.75f)
+            _currentRotation = new Vector3(1f, 0f, 0f);
+        else if(pointing.x < -0.75f)
+            _currentRotation = new Vector3(-1f, 0f, 0f);
+        else if(pointing.y > 0.75f)
+            _currentRotation = new Vector3(0f, 1f, 0f);
+        else if(pointing.y < -0.75f)
+            _currentRotation = new Vector3(0f, -1f, 0f);
+        else if(pointing.z > 0.75f)
+            _currentRotation = new Vector3(0f, 0f, 1f);
+        else if(pointing.z < -0.75f)
+            _currentRotation = new Vector3(0f, 0f, -1f);
+
+        Quaternion start = Camera.transform.localRotation;
+        float startTime = Time.time;
+        float progress = 0f;
+        while(startTime + DELAY_A > Time.time)
+        {
+            yield return null;
+            if(presses != _cameraPresses)
+                yield break;
+            progress += Time.deltaTime;
+            Camera.transform.localRotation = Quaternion.Lerp(start, _trueRotation, progress / DELAY_A);
+        }
+        Camera.transform.localRotation = _trueRotation;
+    }
+
+    private IEnumerator MoveMaze()
+    {
+        Vector3 start = WallsParent.localPosition;
+        Vector3 end = new Vector3(_currentPosition.x, _currentPosition.y, _currentPosition.z) * -0.1f - _currentRotation * 0.1f;
+        float startTime = Time.time;
+        float progress = 0f;
+        while(startTime + DELAY_A > Time.time)
+        {
+            yield return null;
+            progress += Time.deltaTime;
+            WallsParent.localPosition = Vector3.Lerp(start, end, progress / DELAY_A);
+        }
+        WallsParent.localPosition = end;
+    }
+
+    private void AddWall(Vector3Int position, Vector3Int direction, Material m)
+    {
+        GameObject go = Instantiate(WallTemplate, WallsParent);
+        go.transform.localPosition = position;
+        Vector3 rot = DirectionToEuler(direction);
+        go.transform.localEulerAngles = rot;
+        go.transform.localScale = new Vector3(1f, 1f, 1f);
+        go.GetComponentInChildren<CustomMaterialInfo>().Color = m;
+    }
+
+    private Vector3 DirectionToEuler(Vector3Int direction)
+    {
+        Vector3 rot = new Vector3(0f, 0f, 0f);
+        if(direction.x == 1)
+            rot = new Vector3(0f, 0f, 90f);
+        if(direction.x == -1)
+            rot = new Vector3(0f, 0f, -90f);
+        if(direction.y == 1)
+            rot = new Vector3(180f, 0f, 0f);
+        if(direction.y == -1)
+            rot = new Vector3(0f, 0f, 0f);
+        if(direction.z == 1)
+            rot = new Vector3(-90f, 0f, 0f);
+        if(direction.z == -1)
+            rot = new Vector3(90f, 0f, 0f);
+        return rot;
+    }
+
+    private bool BrownButtonPress()
+    {
+        StartCoroutine(AnimateButton(0f, -0.05f));
+        Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.BigButtonPress, transform);
+        if(_moduleActivated && !_moduleSolved)
+        {
+            if(_chosenNet.Any(t => t == _currentPosition + _currentRotation))
+            {
+                if(_moveRoutine != null)
+                    StopCoroutine(_moveRoutine);
+                _moveRoutine = StartCoroutine(MoveMaze());
+                _currentPosition += new Vector3Int((int)_currentRotation.x, (int)_currentRotation.y, (int)_currentRotation.z);
+            }
+            else
+            {
+                if(_absoluteAxes[_currentPosition] == _correctAxis)
+                {
+                    Debug.LogFormat("[Brown's Revenge #{0}] You submitted correctly. Good job!", _moduleId);
+                    Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.CorrectChime, transform);
+                    Module.HandlePass();
+                    _moduleSolved = true;
+                    StartCoroutine(FadeScreen());
+                }
+                else
+                {
+                    Debug.LogFormat("[Brown's Revenge #{0}] You tried to submit at {1}. That is incorrect. Strike!", _moduleId, _currentPosition);
+                    Module.HandleStrike();
+                }
+            }
+        }
+        return false;
+    }
+
+    private IEnumerator FadeScreen()
+    {
+        float time = Time.time;
+        while(Time.time - time < 5f)
+        {
+            WideMazeScreen.material.color = Color.Lerp(new Color(1f, 1f, 1f), new Color(0f, 0f, 0f), (Time.time - time) / 5f);
+            yield return null;
+        }
+        WideMazeScreen.material.color = new Color(0f, 0f, 0f);
+    }
+
+    private void BrownButtonRelease()
+    {
+        StartCoroutine(AnimateButton(-0.05f, 0f));
+        Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.BigButtonRelease, transform);
+    }
+
+    private IEnumerator AnimateButton(float a, float b)
+    {
+        var duration = 0.1f;
+        var elapsed = 0f;
+        while(elapsed < duration)
+        {
+            BrownButtonCap.transform.localPosition = new Vector3(0f, Easing.InOutQuad(elapsed, a, b, duration), 0f);
+            yield return null;
+            elapsed += Time.deltaTime;
+        }
+        BrownButtonCap.transform.localPosition = new Vector3(0f, b, 0f);
+    }
+}
---
+++ Assets/Modules/Brown/CubeNets.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+namespace BrownButton
+{
+    public static class Ex
+    {
+        public static BrownButtonScript.Ax[] TrueCopy(this BrownButtonScript.Ax[] axes)
+        {
+            BrownButtonScript.Ax[] newArr = new BrownButtonScript.Ax[axes.Length];
+            for(int i = 0; i < axes.Length; ++i)
+                newArr[i] = axes[i];
+            return newArr;
+        }
+        public static BrownButtonScript.Ax Opposite(this BrownButtonScript.Ax a)
+        {
+            switch(a)
+            {
+                case BrownButtonScript.Ax.Back:
+                    return BrownButtonScript.Ax.Front;
+                case BrownButtonScript.Ax.Down:
+                    return BrownButtonScript.Ax.Up;
+                case BrownButtonScript.Ax.Front:
+                    return BrownButtonScript.Ax.Back;
+                case BrownButtonScript.Ax.Left:
+                    return BrownButtonScript.Ax.Right;
+                case BrownButtonScript.Ax.Right:
+                    return BrownButtonScript.Ax.Left;
+                case BrownButtonScript.Ax.Up:
+                    return BrownButtonScript.Ax.Down;
+                case BrownButtonScript.Ax.Zag:
+                    return BrownButtonScript.Ax.Zig;
+                case BrownButtonScript.Ax.Zig:
+                    return BrownButtonScript.Ax.Zag;
+            }
+            throw new System.Exception();
+        }
+        public static BrownButtonScript.Ax[] RotateFromTo(this BrownButtonScript.Ax[] axes, BrownButtonScript.Ax a, BrownButtonScript.Ax b)
+        {
+            BrownButtonScript.Ax[] newArr = axes.TrueCopy();
+            int a2 = (int)a.Opposite();
+            int b2 = (int)b.Opposite();
+            newArr[(int)b] = axes[(int)a];
+            newArr[(int)a] = axes[b2];
+            newArr[b2] = axes[a2];
+            newArr[a2] = axes[(int)b];
+            return newArr;
+        }
+        public static BrownButtonScript.Ax[] RotateFromChange(this BrownButtonScript.Ax[] axes, Vector3Int change)
+        {
+            if(change.x == 1)
+                return axes.RotateFromTo(BrownButtonScript.Ax.Right, BrownButtonScript.Ax.Down);
+            if(change.x == -1)
+                return axes.RotateFromTo(BrownButtonScript.Ax.Left, BrownButtonScript.Ax.Down);
+            if(change.y == 1)
+                return axes.RotateFromTo(BrownButtonScript.Ax.Zig, BrownButtonScript.Ax.Down);
+            if(change.y == -1)
+                return axes.RotateFromTo(BrownButtonScript.Ax.Zag, BrownButtonScript.Ax.Down);
+            if(change.z == 1)
+                return axes.RotateFromTo(BrownButtonScript.Ax.Front, BrownButtonScript.Ax.Down);
+            if(change.z == -1)
+                return axes.RotateFromTo(BrownButtonScript.Ax.Back, BrownButtonScript.Ax.Down);
+            throw new System.Exception();
+        }
+    }
+}
